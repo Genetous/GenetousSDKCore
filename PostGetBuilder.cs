2c06f756b3ccad46ce5630cd8d49a4c9ba7954db
omeryil <omer_yildirim@outlook.com.tr>
2024-08-06 09:12:03 +0300

    first commit

---
+++ PostGetBuilder.cs
@@ -0,0 +1,32 @@
+﻿namespace GenetousSDKCore
+{
+    public class PostGetBuilder
+    {
+        PostGet pg = new PostGet();
+        public PostGetBuilder setUrlType(string urlType)
+        {
+            pg.urlType = urlType;
+            return this;
+        }
+        public PostGetBuilder setMethod(string method)
+        {
+            pg.method = method;
+            return this;
+        }
+        public PostGetBuilder setToken(string token)
+        {
+            pg.token = token;
+            return this;
+        }
+        public PostGetBuilder setData(Dictionary<string, object> data)
+        {
+            pg.data = data;
+            return this;
+        }
+       
+        public PostGet build()
+        {
+            return pg;
+        }
+    }
+}
